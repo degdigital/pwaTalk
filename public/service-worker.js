bba445551487cc8ef767c197b81fffb2fb4ea95a
Aaron Ladage <aladage@degdigital.com>
2018-01-23 10:06:56 -0600

    split messaging into its own sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,3 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-
 var cacheName = 'pwaTalk-v1';
 var filesToCache = [
 	'/',
@@ -20,17 +17,6 @@ var filesToCache = [
 	'/images/icons/favicon-96x96.png',
 	'/images/icons/favicon.ico'
 ];
-
-var config = {
-    apiKey: "AIzaSyDcY7PQYjYRHC5pJjjWoYbzcLJX1usk7x4",
-    authDomain: "pwatalk.firebaseapp.com",
-    databaseURL: "https://pwatalk.firebaseio.com",
-    projectId: "pwatalk",
-    storageBucket: "pwatalk.appspot.com",
-    messagingSenderId: "538992840354"
-};
-firebase.initializeApp(config);
-var messaging = firebase.messaging();
 
 self.addEventListener('install', function(e) {
 	console.log('[ServiceWorker] Install');
@@ -66,14 +52,4 @@ self.addEventListener('fetch', function(e) {
 			})
 		);
 	}
-});
-
-messaging.setBackgroundMessageHandler(function(payload) {
-	console.log('[firebase-messaging-sw.js] Received background message ', payload);
-	const title = payload.notification.title;
-	const options = {
-		body: payload.notification.body,
-		icon: '/images/icons/icon-256x256.png'
-	};
-	return self.registration.showNotification(title, options);
 });
